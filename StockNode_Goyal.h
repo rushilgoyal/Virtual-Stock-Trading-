99e0909914aa7effe29e3e4853c4b84aebca26c9
Rushil Goyal <rg761@scarletmail.rutgers.edu>
2017-10-16 16:29:20 -0400

    Create StockNode_Goyal.h

---
+++ StockNode_Goyal.h
@@ -0,0 +1,38 @@
+// Name: Rushil Goyal
+// Final project: Bank and stock account program
+// Date of Submission: 12-23-2016
+
+#pragma once
+#include<iostream>
+#include<string>
+#include "Account_Bhandare.h"
+#include "StockAccount_Bhandare.h"
+using namespace std;
+
+#ifndef STOCKNODE_GOYAL_H
+#define STOCKNODE_GOYAL_H
+
+class StockNode
+{
+	friend class StockAccount;
+
+private:
+	StockNode *nextptr;
+	StockNode *prevptr;
+
+	//Each node contains no. of shares and stock symbol.
+	int numshares;
+	double price_per_share;      // price per share at the time of buying
+	double temp_price_per_share; // temporary price per share at the time of sorting
+	double tempval;              // value of a node = numshares*temp_price_per_share(/price_per_share)
+	string symb;
+
+public:
+	StockNode()
+	{
+		this->nextptr = NULL;
+		this->prevptr = NULL;
+	}
+};
+
+#endif
